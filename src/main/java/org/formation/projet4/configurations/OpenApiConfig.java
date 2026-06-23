b619a7a4e0ef1d2c1faf450658233db3a6899d8d
Mohammed REZGUI <m.rezgui06@gmail.com>
2026-06-23 14:39:25 +0200

    feat: add Open Api support

---
+++ src/main/java/org/formation/projet4/configurations/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package org.formation.projet4.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mon API")
+                        .version("1.0")
+                        .description("Documentation de l'API"));
+    }
+}
